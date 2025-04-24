2b6dde1fd3302c8c3b1ddfd7b28c86bd5fcffb9e
Thaibao123xyz <thaibao123xyz@gmail.com>
2025-04-24 08:19:41 +0700

    MVC_SHOP

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'base' => '/teacher/ss21/',
-    'assets' => '/teacher/ss21/assets/',
-    'baseURL' => 'http://localhost/teacher/ss21/', 
+    'base' => '/MVC_SHOP/MVC_SHOP/',
+    'assets' => '/MVC_SHOP/MVC_SHOP/assets/',
+    'baseURL' => 'http://localhost/MVC_SHOP/MVC_SHOP/',
     'db' => array(
         'host'     => 'localhost',
         'name'     => 'ProductDB',
